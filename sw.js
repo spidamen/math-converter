4ce141c5dc383a17350771f939741000dfcde16c
spidamen <spidaamen.omp@gmail.com>
2026-01-09 15:27:22 +0100

    pwa fix

--- sw.js
+++ sw.js
@@ -1,28 +1,24 @@
-const CACHE_VERSION = 'v2.0'; // Change this with each update!
+const CACHE_VERSION = "v2.0"; // Change this with each update!
 const CACHE_NAME = `converter-cache-${CACHE_VERSION}`;
 
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
     })
   );
   // Force the waiting service worker to become the active service worker
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           // Delete old caches
           if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
+            console.log("Deleting old cache:", cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -33,16 +29,21 @@ self.addEventListener("activate", e => {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+
   e.respondWith(
-    caches.match(e.request).then(res => {
-      // Return cached version or fetch new
-      return res || fetch(e.request).then(fetchRes => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(e.request, fetchRes.clone());
+    caches.match(e.request).then((res) => {
+      return (
+        res ||
+        fetch(e.request).then((fetchRes) => {
+          const resClone = fetchRes.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(e.request, resClone);
+          });
           return fetchRes;
-        });
-      });
+        })
+      );
     })
   );
 });

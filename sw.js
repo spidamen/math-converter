74ff35f27a9ffb07cc6f0fdec7a0cc01dfde5509
spidamen <spidaamen.omp@gmail.com>
2026-01-09 15:04:35 +0100

    manifest and sw update

--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
+const CACHE_VERSION = 'v2.0'; // Change this with each update!
+const CACHE_NAME = `converter-cache-${CACHE_VERSION}`;
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("converter-cache").then(cache => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -8,10 +11,38 @@ self.addEventListener("install", e => {
       ]);
     })
   );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // Delete old caches
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Take control of all pages immediately
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => {
+      // Return cached version or fetch new
+      return res || fetch(e.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(e.request, fetchRes.clone());
+          return fetchRes;
+        });
+      });
+    })
   );
 });
